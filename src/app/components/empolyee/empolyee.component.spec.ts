6741655ec3c71605e26bc7dd0b832d5e356e3363
Syed Umair Ahmed <ahmadumair236@gmail.com>
2024-07-16 06:36:52 +0500

    my second commit

---
+++ src/app/components/empolyee/empolyee.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EmpolyeeComponent } from './empolyee.component';
+
+describe('EmpolyeeComponent', () => {
+  let component: EmpolyeeComponent;
+  let fixture: ComponentFixture<EmpolyeeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [EmpolyeeComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(EmpolyeeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/empolyee/empolyee.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empolyee',
+  standalone: true,
+  imports: [],
+  templateUrl: './empolyee.component.html',
+  styleUrl: './empolyee.component.css'
+})
+export class EmpolyeeComponent {
+
+}
